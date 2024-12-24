033f5b1447457864a1184d3095c40d2872a93d68
Srinivasa Rao Bellamkonda <srenivas35@gmail.com>
2024-12-24 01:57:26 -0800

    updated secrets

--- data/database.js
+++ data/database.js
@@ -5,7 +5,7 @@ const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
 const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
-const dbTestName = process.env.MONGODB_DB_NAME;
+const dbTestName = process.env.MONGODB_DB;
 
 
 const uri = `${connectionProtocol}://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
